092b5ac7980bccb992dd7c86ff627b8b6f8d9b0e
yunusoktay <ynsokty.3@gmail.com>
2023-08-01 16:29:18 +0300

    Day 16
    I learned: @State, two way binding, Form, Section, Group, Picker, NavigationView, ForEach

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by yunus oktay on 1.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//      ContentView.swift contains the initial user interface (UI) for your program, and is where we’ll be doing all the work in this project.
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,19 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by yunus oktay on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//      WeSplitApp.swift contains code for launching your app. If you create something when the app launches and keep it alive the entire time, you’ll put it here.
