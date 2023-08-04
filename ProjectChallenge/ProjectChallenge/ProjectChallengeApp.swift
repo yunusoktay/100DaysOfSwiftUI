82324ed02a2edd044cd487edf8244812fe550a2e
yunusoktay <ynsokty.3@gmail.com>
2023-08-04 22:49:10 +0300

    Day 19
    Challenge day

---
+++ ProjectChallenge/ProjectChallenge/ProjectChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectChallengeApp.swift
+//  ProjectChallenge
+//
+//  Created by yunus oktay on 4.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
