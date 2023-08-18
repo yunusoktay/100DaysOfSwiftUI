706b4b48c56c3ab1c911ad835cd68c569a4b556f
yunusoktay <ynsokty.3@gmail.com>
2023-08-18 23:59:23 +0300

    Inıt

---
+++ Challenge2/Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by yunus oktay on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
