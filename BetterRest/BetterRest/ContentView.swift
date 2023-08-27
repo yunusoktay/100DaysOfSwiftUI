bd2f87d1b5a3d57d7fca0d801fb12b2ed713c1f0
yunusoktay <ynsokty.3@gmail.com>
2023-08-27 16:53:00 +0300

    Day 28
    Project 4, part 3. Challenges completed.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var wakeUp = defaultWakeTime
-    @State private var sleepAmount = 8.0
+    @State private var sleepAmount = 8
     @State private var coffeeAmount = 1
     
     @State private var alertTile = ""
@@ -29,29 +29,39 @@ struct ContentView: View {
        
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section(header: Text("When do you want to wake up?")
+                    .font(.headline)) {
                     
                     DatePicker ("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
+                Section() {
                     
+                    Picker("Select sleep amount", selection: $sleepAmount) {
+                        ForEach(4...12, id: \.self) { hours in
+                            Text("\(hours.formatted()) hours")
+                        }
+                    }
+                    
+                } header: {
                     Text("Desired amount of sleep")
                         .font(.headline)
-                    
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    
-                    Text("Daily coffee intake")
-                        .font(.headline)
+                Section(header: Text("Daily coffee intake")
+                    .font(.headline)) {
                     
                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20 )
+                }
+                
+                Section(header: Text("Your recommended bedtime")
+                        .font(.headline)
+                        .foregroundColor(.black)) {
+                    Text(alertMessage)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
                 }
             }
             
@@ -78,7 +88,7 @@ struct ContentView: View {
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: Double(sleepAmount), coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUp - prediction.actualSleep
             alertTile = "Your ideal bedtime is.."
